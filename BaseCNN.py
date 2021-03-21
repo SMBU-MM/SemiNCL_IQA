e399c0f375394f69d41338f23aa3f604973dba38
WANG Zhihua <1072020701@qq.com>
2021-03-22 02:25:57 +0800

    batchnorm1d in FC
    remove feature normalization and add  batchnorm1d after FC. In batch normalization, the affine is set to False. This means gamma and beta is set to 1 and 0, respectively, all the time

--- BaseCNN.py
+++ BaseCNN.py
@@ -14,6 +14,7 @@ class BaseCNN(nn.Module):
         self.backbone3 = models.resnet18(pretrained=True)
         self.backbone4 = models.resnet18(pretrained=True)
         if self.config.fz:
+        #if True:
             # Freeze all previous layers.
             for key, param in self.backbone1.named_parameters():
                 param.requires_grad = False
@@ -26,21 +27,34 @@ class BaseCNN(nn.Module):
             # Freeze all previous layers.
             for key, param in self.backbone4.named_parameters():
                 param.requires_grad = False
+                
         outdim = 1
-        self.backbone1.fc = nn.Linear(512, outdim)
-        self.backbone2.fc = nn.Linear(512, outdim)
-        self.backbone3.fc = nn.Linear(512, outdim)
-        self.backbone4.fc = nn.Linear(512, outdim)
+        self.backbone1.fc = nn.Linear(512, outdim, bias=False)
+        self.backbone2.fc = nn.Linear(512, outdim, bias=False)
+        self.backbone3.fc = nn.Linear(512, outdim, bias=False)
+        self.backbone4.fc = nn.Linear(512, outdim, bias=False)
+
         # Initialize the fc layers.
         nn.init.kaiming_normal_(self.backbone1.fc.weight.data)
         nn.init.kaiming_normal_(self.backbone2.fc.weight.data)
         nn.init.kaiming_normal_(self.backbone3.fc.weight.data)
         nn.init.kaiming_normal_(self.backbone4.fc.weight.data)
         if self.backbone1.fc.bias is not None:
-            nn.init.constant_(self.backbone1.fc.bias.data, val=0)
-            nn.init.constant_(self.backbone2.fc.bias.data, val=0)
-            nn.init.constant_(self.backbone3.fc.bias.data, val=0)
-            nn.init.constant_(self.backbone4.fc.bias.data, val=0)
+            nn.init.constant_(self.backbone1.fc1.bias.data, val=0)
+            nn.init.constant_(self.backbone2.fc1.bias.data, val=0)
+            nn.init.constant_(self.backbone3.fc1.bias.data, val=0)
+            nn.init.constant_(self.backbone4.fc1.bias.data, val=0)
+        
+        # euqal to share gamma
+        self.share_fc = nn.Linear(outdim, outdim, bias=False)
+        nn.init.kaiming_normal_(self.share_fc.weight.data)
+        if self.share_fc.bias is not None:
+            nn.init.constant_(self.share_fc.bias.data, val=0)
+
+        self.backbone1.fcbn = nn.BatchNorm1d(outdim, affine=False)
+        self.backbone2.fcbn = nn.BatchNorm1d(outdim, affine=False)
+        self.backbone3.fcbn = nn.BatchNorm1d(outdim, affine=False)
+        self.backbone4.fcbn = nn.BatchNorm1d(outdim, affine=False)
 
     def forward(self, x):
         """
@@ -57,8 +71,9 @@ class BaseCNN(nn.Module):
         x1 = self.backbone1.layer3(x_share)
         x1 = self.backbone1.layer4(x1)
         x1 = self.backbone1.avgpool(x1).view(x.size(0), -1)
-        x1 = F.normalize(x1, p=2, dim=1)
         x1 = self.backbone1.fc(x1)
+        x1 = self.share_fc(x1)
+        x1 = self.backbone1.fcbn(x1)
         mean1 = x1[:, 0]
         #t = x1[:, 1]
         #var1 = nn.functional.softplus(t)**2
@@ -68,8 +83,10 @@ class BaseCNN(nn.Module):
         x2 = self.backbone2.layer3(x_share)
         x2 = self.backbone2.layer4(x2)
         x2 = self.backbone2.avgpool(x2).view(x.size(0), -1)
-        x2 = F.normalize(x2, p=2, dim=1)
+        #x2 = F.normalize(x2, p=2, dim=1)
         x2 = self.backbone2.fc(x2)
+        x2 = self.share_fc(x2)
+        x2 = self.backbone2.fcbn(x2)
         mean2 = x2[:, 0]
         #t = x2[:, 1]
         #var2 = nn.functional.softplus(t)**2
@@ -79,8 +96,10 @@ class BaseCNN(nn.Module):
         x3 = self.backbone3.layer3(x_share)
         x3 = self.backbone3.layer4(x3)
         x3 = self.backbone3.avgpool(x3).view(x.size(0), -1)
-        x3 = F.normalize(x3, p=2, dim=1)
+        #x3 = F.normalize(x3, p=2, dim=1)
         x3 = self.backbone3.fc(x3)
+        x3 = self.share_fc(x3)
+        x3 = self.backbone3.fcbn(x3)
         mean3 = x3[:, 0]
         #t = x3[:, 1]
         #var3 = nn.functional.softplus(t)**2
@@ -92,10 +111,11 @@ class BaseCNN(nn.Module):
         x4 = self.backbone4.avgpool(x4).view(x.size(0), -1)
         x4 = F.normalize(x4, p=2, dim=1)
         x4 = self.backbone4.fc(x4)
+        x4 = self.share_fc(x4)
+        x4 = self.backbone4.fcbn(x4)
         mean4 = x4[:, 0]
         #t = x4[:, 1]
         #var4 = nn.functional.softplus(t)**2
         var4 = torch.ones_like(mean4)**2
         return [mean1, mean2, mean3, mean4], [var1, var2, var3, var4]
    
-
